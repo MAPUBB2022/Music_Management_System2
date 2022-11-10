ea1b24ff65c520bac92c06bfc52c5d0c5f0b6dfd
kgypeter <kgyulapeter10@gmail.com>
2022-11-10 15:52:59 +0200

    test

---
+++ src/test/AdminControllerTest.java
@@ -0,0 +1,137 @@
+package test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminControllerTest {
+    @BeforeEach
+    void setUP(){
+
+    }
+
+    @Test
+    void addAlbum() {
+    }
+
+    @Test
+    void deleteAlbum() {
+    }
+
+    @Test
+    void modifyAlbum() {
+    }
+
+    @Test
+    void addArtist() {
+    }
+
+    @Test
+    void deleteArtist() {
+    }
+
+    @Test
+    void modifyArtist() {
+    }
+
+    @Test
+    void addBand() {
+    }
+
+    @Test
+    void deleteBand() {
+    }
+
+    @Test
+    void modifyBand() {
+    }
+
+    @Test
+    void addConcert() {
+    }
+
+    @Test
+    void deleteConcert() {
+    }
+
+    @Test
+    void modifyConcert() {
+    }
+
+    @Test
+    void addMusicLabel() {
+    }
+
+    @Test
+    void deleteMusicLabel() {
+    }
+
+    @Test
+    void modifyMusicLabel() {
+    }
+
+    @Test
+    void addSong() {
+    }
+
+    @Test
+    void deleteSong() {
+    }
+
+    @Test
+    void modifySong() {
+    }
+
+    @Test
+    void addUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void modifyUser() {
+    }
+
+    @Test
+    void sortAlbumsByRevenue() {
+    }
+
+    @Test
+    void sortSongsByRating() {
+    }
+
+    @Test
+    void sortSongsByReleaseDate() {
+    }
+
+    @Test
+    void sortArtistsByName() {
+    }
+
+    @Test
+    void sortAlbumsByReleaseDate() {
+    }
+
+    @Test
+    void sortAlbumsByProductionCost() {
+    }
+
+    @Test
+    void showConcerts() {
+    }
+
+    @Test
+    void showArtists() {
+    }
+
+    @Test
+    void showAlbums() {
+    }
+
+    @Test
+    void showUpcomingConcerts() {
+    }
+}
---
+++ src/test/UserControllerTest.java
@@ -0,0 +1,65 @@
+package test;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @BeforeEach
+    void setUP(){
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void showArtists() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void showAlbums() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void showUpcomingConcerts() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sortAlbumsByRevenue() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sortSongsByRating() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sortSongsByReleaseDate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sortArtistsByName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sortAlbumsByReleaseDate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addFavourite() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeFavourite() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void showFavourites() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void buyTicket() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void showRecommended() {
+    }
+}
